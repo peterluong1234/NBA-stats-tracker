280d2976ab0186d36a16ef662abb2e57d6f3d46f
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-04-10 12:10:59 -0700

    Added login functionality

--- models/user.js
+++ models/user.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+	name: String,
+	googleId: {
+	  type: String,
+	  required: true
+	},
+	email: String
+  }, {
+	timestamps: true
+  });
+
+module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res) {
   // in the student demo this was res.redirect('/students'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
+  res.render('index');
 });
 
 // Google OAuth login route
@@ -23,6 +24,19 @@ router.get('/oauth2callback', passport.authenticate(
     failureRedirect : '/' // where do you want the client to go if login fails
   }
 ));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google', { failureredirect : '/users/login'}),
+  function(req, res) {
+    if(req.user.createdAt === req.user.updatedAt){
+      res.redirect(`/users/register/${req.user._id}`)
+    }
+    else{
+      res.redirect('/posts')
+    }
+  }
+)
+
 
 // OAuth logout route
 router.get('/logout', function(req, res){
