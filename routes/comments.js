d0d0a0232e04b6f9a7f6565a324e1a2ff0127cac
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-04-27 22:09:52 -0700

    routes/comments.js -> added 'isLoggedIn' to routes

--- routes/comments.js
+++ routes/comments.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 const playersCtrl = require('../controllers/players');
+const isLoggedIn = require('../config/auth');
 // const request = require('request');
 // const rootURL = 'https://www.balldontlie.io/';
 
 // router.get('/', playersCtrl.index);
-router.post('/players/:id/comments', commentsCtrl.create);
-router.get('/comments/:id', commentsCtrl.show);
-router.delete('/comments/:id', commentsCtrl.delete);
-router.put('/comments/:id', commentsCtrl.update);
+router.post('/players/:id/comments', isLoggedIn, commentsCtrl.create);
+router.get('/comments/:id', isLoggedIn, commentsCtrl.show);
+router.delete('/comments/:id', isLoggedIn, commentsCtrl.delete);
+router.put('/comments/:id', isLoggedIn, commentsCtrl.update);
 
 module.exports = router;
